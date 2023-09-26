fff94a47345d7190c78b1ce7b5322f8ee3ba7c20
TalhaBayansar <talha.bayansar@ordina.be>
2023-09-26 12:31:50 +0200

    adding basic header layout for desktop

---
+++ utils/constants.ts
@@ -0,0 +1,12 @@
+export const swUrl = "https://swapi.dev/api";
+
+export enum API_URLS {
+  films = `${swUrl}/films`,
+  people = `${swUrl}/people`,
+}
+
+export enum APP_URLS {
+  home = "/",
+  movies = "/movies",
+  characters = "/characters",
+}
