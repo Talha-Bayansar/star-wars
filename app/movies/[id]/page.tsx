f6929582b2b5e7f379303a2d0f2ad419554a0aae
TalhaBayansar <talha.bayansar@ordina.be>
2023-09-27 21:46:10 +0200

    adding character card and routing to details page of character

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,6 +1,8 @@
+import { Character } from "@/characters";
 import { Movie } from "@/movies";
-import { API_URLS } from "@/utils";
+import { API_URLS, APP_URLS } from "@/utils";
 import axios from "axios";
+import Link from "next/link";
 
 export default async function MovieDetails({
   params,
@@ -9,10 +11,10 @@ export default async function MovieDetails({
 }) {
   const response = await axios.get<Movie>(`${API_URLS.films}/${params.id}`);
   const movie = response.data;
-  const characters: any[] = [];
+  const characters: Character[] = [];
   for (let index = 0; index < movie.characters.length; index++) {
     const url = movie.characters[index];
-    const character = await axios.get(url);
+    const character = await axios.get<Character>(url);
     characters.push(character.data);
   }
 
@@ -21,10 +23,24 @@ export default async function MovieDetails({
       <h1 className="text-4xl sm:text-6xl font-semibold">{movie.title}</h1>
       <p>{movie.opening_crawl}</p>
       <h2 className="text-2xl sm:text-4xl font-semibold">Characters</h2>
-      <div className="grid grid-cols-3">
-        {characters.map((v) => (
-          <div key={v.name}>{v.name}</div>
-        ))}
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+        {characters.map((v) => {
+          const id = v.url.split("/")[v.url.split("/").length - 2];
+          return (
+            <div key={v.name} className="flex flex-col gap-2 shadow-lg p-4">
+              <Link href={`${APP_URLS.characters}/${id}`}>
+                <h3 className="text-xl font-medium hover:text-orange-400">
+                  {v.name}
+                </h3>
+              </Link>
+              <div className="flex flex-col gap-1">
+                {v.films.map((movie) => (
+                  <span key={movie}>{movie}</span>
+                ))}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </main>
   );
