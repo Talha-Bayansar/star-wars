0ab07ed5af42fb08483438b3b987ef07b52c6c67
TalhaBayansar <talha.bayansar@ordina.be>
2023-10-02 20:31:33 +0200

    fix pagination

--- app/characters/page.tsx
+++ app/characters/page.tsx
@@ -1,84 +1,46 @@
-import { Character, CharacterCard } from "@/characters";
-import { API_URLS, APP_URLS } from "@/utils";
-import axios from "axios";
-import Link from "next/link";
-import { ReactNode } from "react";
-import { twMerge } from "tailwind-merge";
+"use client";
 
-type ApiResponse = {
-  count: number;
-  next?: string;
-  previous?: string;
-  results: Character[];
-};
+import { CharacterCardSkeleton } from "@/characters";
+import { CharactersView } from "@/characters/components/CharactersView";
+import { Suspense } from "react";
 
-export const revalidate = 0;
-
-export default async function Characters({
+export default function Characters({
   searchParams,
 }: {
   searchParams: { page?: string };
 }) {
-  const currentPage = parseInt(searchParams.page ?? "1");
-  const { data } = await axios.get<ApiResponse>(
-    `${API_URLS.people}?page=${currentPage}`
-  );
-  const characters = data.results;
-
-  const pageCount = Math.ceil(data.count / 10);
-  const previousPage = currentPage > 1 ? currentPage - 1 : null;
-  const nextPage = currentPage < pageCount ? currentPage + 1 : null;
-
-  const countArray = [];
-  for (let index = 0; index < pageCount; index++) {
-    countArray.push(index + 1);
-  }
-
   return (
     <main className="flex flex-col gap-8 p-4">
-      <div className="flex items-center border border-gray-100 p-4">
-        <CustomLink page={previousPage}>Previous</CustomLink>
-        <div className="flex flex-grow justify-center items-center gap-2">
-          {countArray.map((count) => (
-            <CustomLink
-              key={`pages_${count}`}
-              page={count}
-              className={`w-auto ${
-                currentPage === count ? "text-orange-300" : "text-black"
-              }`}
-            >
-              {count}
-            </CustomLink>
-          ))}
-        </div>
-        <CustomLink page={nextPage}>Next</CustomLink>
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-        {characters.map((character) => {
-          return <CharacterCard key={character.name} character={character} />;
-        })}
-      </div>
+      <Suspense fallback={<Skeleton />}>
+        <CharactersView page={searchParams.page} />
+      </Suspense>
     </main>
   );
 }
 
-interface ICustomLink {
-  children: ReactNode;
-  page?: number | null;
-  className?: string;
-}
-
-const CustomLink = ({ children, page, className }: ICustomLink) => {
-  if (page) {
-    return (
-      <Link
-        href={`?page=${page}`}
-        className={twMerge("hover:text-orange-400", className)}
-      >
-        {children}
-      </Link>
-    );
-  } else {
-    return <div className={twMerge("", className)} />;
-  }
+const Skeleton = () => {
+  return (
+    <>
+      <div className="flex items-center border border-gray-100 p-4">
+        <div className="h-4 w-16 bg-gray-400" />
+        <div className="flex flex-grow justify-center items-center gap-2">
+          <div className="w-4 h-4 bg-gray-300" />
+          <div className="w-4 h-4 bg-gray-300" />
+          <div className="w-4 h-4 bg-gray-300" />
+        </div>
+        <div className="h-4 w-16 bg-gray-400" />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        <CharacterCardSkeleton />
+        <CharacterCardSkeleton />
+        <CharacterCardSkeleton />
+        <CharacterCardSkeleton />
+        <CharacterCardSkeleton />
+        <CharacterCardSkeleton />
+        <CharacterCardSkeleton />
+        <CharacterCardSkeleton />
+        <CharacterCardSkeleton />
+      </div>
+    </>
+  );
 };
---
+++ characters/components/CharactersView.tsx
@@ -0,0 +1,95 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+import { CharacterCard } from ".";
+import axios from "axios";
+import { API_URLS } from "@/utils";
+import { Character } from "..";
+
+type ApiResponse = {
+  count: number;
+  next?: string;
+  previous?: string;
+  results: Character[];
+};
+
+const fetchData = async (page?: number) => {
+  const currentPage = page ?? 1;
+  const { data } = await axios.get<ApiResponse>(
+    `${API_URLS.people}?page=${currentPage}`
+  );
+  const characters = data.results;
+
+  const pageCount = Math.ceil(data.count / 10);
+  const previousPage = currentPage > 1 ? currentPage - 1 : null;
+  const nextPage = currentPage < pageCount ? currentPage + 1 : null;
+
+  const countArray = [];
+  for (let index = 0; index < pageCount; index++) {
+    countArray.push(index + 1);
+  }
+
+  return {
+    characters,
+    countArray,
+    nextPage,
+    previousPage,
+    currentPage,
+  };
+};
+
+type Props = {
+  page?: string;
+};
+
+export const CharactersView = async ({ page }: Props) => {
+  const { characters, countArray, nextPage, previousPage, currentPage } =
+    await fetchData(parseInt(page ?? "1"));
+
+  return (
+    <>
+      <div className="flex items-center border border-gray-100 p-4">
+        <CustomLink page={previousPage}>Previous</CustomLink>
+        <div className="flex flex-grow justify-center items-center gap-2">
+          {countArray.map((count) => (
+            <CustomLink
+              key={`pages_${count}`}
+              page={count}
+              className={`w-auto ${
+                currentPage === count ? "text-orange-300" : "text-black"
+              }`}
+            >
+              {count}
+            </CustomLink>
+          ))}
+        </div>
+        <CustomLink page={nextPage}>Next</CustomLink>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        {characters.map((character) => {
+          return <CharacterCard key={character.name} character={character} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+interface ICustomLink {
+  children: ReactNode;
+  page?: number | null;
+  className?: string;
+}
+
+const CustomLink = ({ children, page, className }: ICustomLink) => {
+  return (
+    <Link
+      href={`/characters?page=${page}`}
+      className={twMerge(
+        `hover:text-orange-400 ${!page && "pointer-events-none opacity-50"}`,
+        className
+      )}
+    >
+      {children}
+    </Link>
+  );
+};
