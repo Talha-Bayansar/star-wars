4e22db63c3dbf2f3083cd3703842434234c0d577
TalhaBayansar <talha.bayansar@ordina.be>
2023-10-04 00:53:29 +0200

    fix env var vor root url

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,5 @@
 export const swUrl = "https://swapi.dev/api";
-export const rootUrl = process.env.ROOT_URL;
+export const rootUrl = process.env.NEXT_PUBLIC_ROOT_URL;
 const rootApiUrl = `${rootUrl}/api`;
 
 export enum API_URLS {
